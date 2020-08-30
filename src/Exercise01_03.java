343d173031b3f00cc6c478828c9cd6df00458ed7
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-08-30 12:55:54 -0700

    f

---
+++ src/Exercise01_03.java
@@ -0,0 +1,11 @@
+
+public class Exercise01_03 {
+	public static void main(String[] args) {
+		System.out.println("    J     A     V     V    A     \r\n    J    A A     V   V    A A    \r\nJ   J   AAAAA     V V    AAAAA   \r\nJ J   A     A     V    A     A  ");
+//		System.out.println("    J     A     V     V    A     ");
+//		System.out.println("    J    A A     V   V    A A    ");
+//		System.out.println("J   J   AAAAA     V V    AAAAA   ");
+//		System.out.println(" J J   A     A     V    A     A  ");
+//		
+	}
+}
